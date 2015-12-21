3e1ca28b6ab3211bf0b4b036a039de5dd8c31d9e
spencermeyer <spencer_meyer@hotmail.com>
2015-12-21 23:02:53 +0000

    got the view working needs more styling but ajax works

--- public/scraper.js
+++ public/scraper.js
@@ -3,19 +3,23 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
-
   console.log("hello from scraper.js required in index html");
-
 
   $.getJSON("example.json", function(result){
     console.log("got some jason ? ");
     console.log(result);
 
-    var htmltoappend = '<table id="results">'
+    var htmltoappend = '<table id="results" class="table-bordered">'
     htmltoappend = htmltoappend + '<tbody>'
-    htmltoappend = htmltoappend + '<tr><td>' + result.pos + '</td><td>' + result.parkrunner + '</td>' + '<td>' + result.time + '</td>' + '<td>' + result.agecat + '</td>' + '</tr>'
-    htmltoappend = htmltoappend + '</tbody>' + '</table>'
-    console.log("and htmltoappend is: ", htmltoappend);
+    htmltoappend = htmltoappend + '<tr><th>' + "Result" + '</th><th>' + 'Parkrunner' + '</th>' + '<th>' + 'Time' + '</th>' + '<th>' + 'Agecat' + '</th>' + '<th>' + 'Agegrade' + '<th>'+'Gender' + '</th>' + '<th>' + 'GenderPos' + '</th>' + '<th>' + 'Note' + '</th>' + '<th>' + 'Total Runs' + '</th></tr>';
+
+
+    for (i = 0; i < result.length; i++) {
+      htmltoappend = htmltoappend + '<tr><td>' + result[i].pos + '</td><td>' + result[i].parkrunner + '</td>' + '<td>' + result[i].time + '</td>' + '<td>' + result[i].agecat + '</td>' + '<td>' + result[i].agegrade + '</td>' + '<td>' + result[i].gender + '</td>' + '<td>' + result[0].genderpos + '</td>' + '<td>' + result[i].Note + '</td>' + '<td>' + result[0].totalruns + '</th>' + '</tr>';
+    }
+
+    htmltoappend = htmltoappend + '</tbody>' + '</table>';
+
     $('#inject_here').append(htmltoappend);
   });
 
