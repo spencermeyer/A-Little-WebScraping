cecca7d29c41d9ad79a5c42837a722189003d716
spencermeyer <spencer_meyer@hotmail.com>
2016-01-16 20:03:49 +0000

    For function is working but has problems of working too fast before async file read and write

--- README.md
+++ README.md
@@ -8,6 +8,20 @@ I upload the JSON and inject it into the view as a table.
 
 The injector code is fairly DRY but scraping code wants refactoring as I repeat too much code, this is work in progress.
 
+http://blog.miguelgrinberg.com/post/easy-web-scraping-with-nodejs
+
+https://scotch.io/tutorials/scraping-the-web-with-node-js
+
+This is my WebScraping example.
+
+
 This application is deployed on openshift:
 
 http://parkcollector-systraph.rhcloud.com/
+
+
+to run:  node server.js    
+or
+
+don't use nodemon server.js   because the json file changes in the middle of the routine and it thinks it is a file change and re-starts.
+
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/results', function(req, res){
 });
 
 // this route scrapes the consolodated site and makes a json of the sources
+// This is working!!!
 app.get('/scrape2', function(req, res){
   var options = {
     url : 'http://localhost:8000/results_Consolidated_parkrun.html',
@@ -38,7 +39,6 @@ app.get('/scrape2', function(req, res){
       'User-Agent': 'request'
     }
   };
-  console.log("hello from scrape 2");
   request(options, function(error, response, html){
     if(error){console.log('There was an error', error)};
     if(!error){console.log('not an error!')}
@@ -49,20 +49,16 @@ app.get('/scrape2', function(req, res){
     $('.floatleft a').not('.sortable').each(function(i, element){
       test=element.attribs.href;
       if(test.indexOf("weekly")!=-1){
-        console.log("one element but how many?", test, i);
+        console.log("json push this:", test, i);
         json.push({"website": test});
       }
     });
     // now the json has the links to each parkrun, write the file
     fs.writeFile('public/links.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File successfully written! - Check your project directory for the links.json file');
+      console.log('File links.json successfully written! - Check your project directory for the links.json file');
     });
-
-    console.log('after the selection');
   });
-
   console.log("hello from scrape2 after loading file");
-
   res.sendfile('./public/results2.html'); 
 });
 
@@ -70,63 +66,73 @@ app.get('/scrape2', function(req, res){
 // this route does the scraping and saves to json
 app.get('/scrape', function(req, res){
   // Let's scrape
-
+  console.log("from scrape");
+  // First clean the output.json
+  var json =[];
+  //  fs.writeFile('public/output.json', JSON.stringify(json, null, 4), function(err){  
+  //    console.log('output.json is cleaned');  
+  //  });
+  //  OK THIS WORKED BUT WAS ASYNC.
+  fs.writeFileSync('public/output.json', JSON.stringify(json, null, 4));
+  console.log("json cleaned / created");
+  
   var sitesLocal = [
   { "website" : 'http://localhost:8000/results_Eastleigh_parkrun.html'},
   { "website" : 'http://localhost:8000/results_Netley_Abbey_parkrun.html'},
   { "website" : 'http://localhost:8000/results_Southampton_parkrun.html'},
   { "website" : 'http://localhost:8000/results_Winchester_parkrun.html'}
   ]
-
 //  ****  Lets scrape all
-// First clean the output.json
-fs.writeFile('public/output.json', JSON.stringify(json, null, 4), function(err){  
-  console.log('Output.json is cleaned');  
-});
 
 // PLAN : 
 // 1) test this loop and debug,
 // 2) make it append to json not separate files  OR make a deeper JSON object 
 //  like this   { 1:{stuff from one website}, 2:{stuff from another}  }
-// 3) add a reference to the parkrun location and date into json  scrape h2
-
+// 3) add a reference to the parkrun location and date into json scrape h2
 
 // now go through all the websites where there are results:
-  for (var i=0, len=sitesLocal.length; i<len; i++){
-    console.log('scraping:',sitesLocal[i].website);  
-    var options = {
+
+var options = {
       // url : 'http://www.parkrun.org.uk/eastleigh/results/latestresults/',
-      url : sitesLocal[i].website,
+      url : sitesLocal[0].website,
       headers: {
         'User-Agent': 'request'
       }
     };
+    console.log('after set options');
+  // THIS IS THE LOOP
+  // lets try a promise
+
+  for (i=0; i<(sitesLocal.length); i++){
+    console.log('scraping:', i, sitesLocal[i].website);  
+    options.url = sitesLocal[i].website;
+      // This works, but console logs are deceiving writefile website wrong
+      // because file write is still happening as program ex continues
+      // and it writes data that is changing
       request(options, function(error, response, html){
         if(error){console.log('There was an error', error)};
         if(!error){
+          console.log("first thing from the no error", i);
           var $ = cheerio.load(html);
-          console.log('loaded webpage',sitesLocal[i], 'pr no errors');
+          console.log('loaded webpage', options.url, 'parkrun no errors');
           var json =[];
-  
+          //Here, pick out the data and assign json
           $('table.sortable tbody tr').each(function(i, element){ 
             var children = $(this).children();
             children.each(function(){
               if(children.eq(7).text() === "Eastleigh RC"){
-                json[children.eq(0).text()] = { "pos" : children.eq(0).text(), "parkrunner" : children.eq(1).text(), "  time": children.eq(2).text(), "agecat" :  children.eq(3).text(), "agegrade" : children.eq(4).text() , "gender" : children.eq(5).text(), "genderpos" : children.eq(6).text(), "club" : children.eq(7).text(), "Note" : children.eq(8).text(), "TotalRuns" : children.eq(9).text() };  
-            } 
-          });    
-        }); 
-      } 
-      fs.writeFile('public/output.json', JSON.stringify(json, null, 4), function(err){  
-        console.log('File successfully written! - Check your project directory for the output.json file');  
-      });
-    });
-  }
-
-
-
-
-
+                json[children.eq(0).text()] = { "parkrun" : options.url, "pos" : children.eq(0).text(), "parkrunner" :   children.eq(1).text(), "  time": children.eq(2).text(), "agecat" :  children  .eq(3). text(), "agegrade" : children.eq(4).text() , "gender" : children.eq(5).text( ), " genderpos" : children.eq(6).text(), "club" : children.eq(7).text(), "Note" :    children.eq(8).text(), "TotalRuns" : children.eq(9).text() };   
+              }   
+            });       
+          });   
+        }
+    //  make it wait
+    fs.appendFileSync('public/output.json', JSON.stringify(json, null, 4));  
+    console.log('File sync written! - Check your output.json file');
+        //this working
+      });  //end of request
+}
+console.log("and this is after the subroutine before file send");
 res.sendfile('./public/results.html');
 });
 
