af2f0d1c66d8202fed8174e636a3fd459f0accee
spencermeyer <spencer_meyer@hotmail.com>
2016-01-23 22:51:11 +0000

    splitting run id started

--- public/injector.js
+++ public/injector.js
@@ -7,11 +7,13 @@ $( document ).ready(function() {
     console.log("should be 1.5 seconds after loading");
     $.getJSON("output.json", function (result) {
       console.log('got some json?', result.length);
-      var htmltoappend = '<table id="results" class="table-bordered">'
-      htmltoappend = htmltoappend + '<tbody>'
+      var htmltoappend = '<table id="results" class="table table-bordered">'
+      htmltoappend = htmltoappend + '<thead>'
       htmltoappend = htmltoappend + '<tr><th>'+'Parkrun'+'</th><th>' + "Result" + '</th><th>' + 'Parkrunner' + '</th>' + '<th>' + 'Time' + '</th>' + '<th>' + 'Agecat' + '</th>' + '<th>' + 'Agegrade' + '<th class="gender">'+'Gender' + '</th>' +  '<th>' + 'GenderPos' + '</th>' + '<th>' + 'Note' + '</th>' + '<th>' + 'Total Runs' + '</th></tr>';
+      htmltoappend = htmltoappend + '</thead><tbody>'
       for (i = 0; i < result.length; i++) {
-        htmltoappend = htmltoappend + '<tr>' + '<td>' + result[i].parkrun + '</td><td>' + result[i].pos + '</td><td>' + result[i].parkrunner + '</td>' + '<td>' + result[i].time + '</td>' + '<td>' + result[i].agecat + '</td>' + '<td>' + result[i].agegrade + '</td>' + '<td class="gender">' + result[i].gender + '</td>' + '<td>' + result[i].genderpos + '</td>' + '<td>' + result[i].Note + '</td>' + '<td>' + result[i].TotalRuns + '</th>' + '</tr>';
+        htmltoappend = htmltoappend + '<tr>' + '<td>' + result[i].parkrun.split(" ")[0] + '</td><td>' + result[i].pos + '</td><td>' + result[i].parkrunner + '</td>' + '<td>' + result[i].time + '</td>' + '<td>' + result[i].agecat + '</td>' + '<td>' + result[i].agegrade + '</td>' + '<td class="gender">' + result[i].gender + '</td>' + '<td>' + result[i].genderpos + '</td>' + '<td>' + result[i].Note + '</td>' + '<td>' + result[i].TotalRuns + '</th>' + '</tr>';
+        console.log(result[i].parkrun.split(" ")[0]);
       }
       htmltoappend = htmltoappend + '</tbody>' + '</table>';
       $('#inject_here').append(htmltoappend);
