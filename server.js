50d530036a7c95bf16b3d58aec882f5b9784fca1
spencermeyer <spencer_meyer@hotmail.com>
2016-02-12 08:45:56 +0000

    got first callback working to links now to do second

--- server.js
+++ server.js
@@ -23,24 +23,6 @@ app.get('/results', function(req, res){
   res.sendfile('./public/results.html');
 });
 
-// OK HERE'S THE CALLBACK IDEA:
-//
-//  getLinks(URL, scrapeAndWrite);
-//  
-//  function getLinks(indexURL, callback){
-//    listOfLinks=blah blah blah;
-//    callback(listOfLinks);    
-//  }
-//
-//  function scrapeAndWrite(linksForScraping) {
-//    scrape all of linksForScraping;
-//    write to file;      **POTENTIAL PROB NEED ANOTHER CALLBACK?
-// }
-//
-//
-
-
-
 // this route scrapes, makes a json and sends the results view
 app.get('/scrape', function(req, res){
   var options = {
@@ -51,75 +33,38 @@ app.get('/scrape', function(req, res){
     }
   };
   var linksjson =[];
-  request(options, function(error, response, html){
-    if(error){console.log('There was an error', error)};
-    if(!error){console.log('not an error!')}
+
+
+  getLinks(options, writeLinks);
+
+  function getLinks(linksSources, returnLinks){
+    request(linksSources, function(error, response, html){
+     if(error){console.log('There was an error', error)};
+     if(!error){console.log('not an error!')}
       console.log("something from the request to consolidated");
-    var $ = cheerio.load(html);
-    console.log('loaded webpage consolodated no errors');
-    $('.floatleft a').not('.sortable').each(function(i, element){
-      test=element.attribs.href;
-      if(test.indexOf("weekly")!=-1){
-        console.log("linksjson push this:", test, i);
-        linksjson.push({"website": test});
-      }
-    });
-    // now the linksjson has the links to each parkrun, write the file
-    fs.writeFile('public/links.json', JSON.stringify(linksjson, null, 4), function(err){
-      console.log('File links.json successfully written! - Check your project directory for the links.json file');
-    });
-  });  // end of the request routine
-
-var timerFunction0 = setTimeout(function(){
-// going to try 500 ms timeout of this first time function
-  // Let's scrape
-  console.log("from individual scrapes");
-  // First clean the output.json
-  var json =[];
-  fs.writeFileSync('public/output.json', JSON.stringify(json, null, 4));
-  console.log("json cleaned / created");
-  
-// now go through all the websites where there are results:
-var options = {
-      url : linksjson[0].website,
-      headers: {
-        'User-Agent': 'request'
-      }
-    };
-    console.log('after set options');
-
-  // THIS IS THE LOOP
-  for(website in linksjson)
-  { 
-    options.url = linksjson[website].website;
-      request(options, function(error, response, html){
-        if(error){console.log('There was an error', error)};
-        if(!error){
-          console.log("no error, scraping");
-          var $ = cheerio.load(html);
-          //Here, pick out the data and assign json
-          $('table.sortable tbody tr').each(function(i, element){ 
-            var children = $(this).children();
-            if(children.eq(7).text() === "Eastleigh RC"){
-              json.push({ "parkrun" : $('#primary h2').text(), "pos" : children.eq(0).text(), "parkrunner" :  children.eq(1).text(), "time": children.eq(2).text(), "agecat" : children.eq(3).text(), "agegrade" : children.eq(4).text(), "gender" : children.eq(5).text(), "genderpos" : children.eq(6).text(), "club" : children.eq(7).text(), "Note" : children.eq(8).text(), "TotalRuns" : children.eq(9).text()});   
-            }   
-          }); // end of each element in table sortable 
-          console.log('here the file is read and json assigned');
-          console.log("");
-         }
+      var $ = cheerio.load(html);
+      console.log('loaded webpage consolodated no errors');
+      $('.floatleft a').not('.sortable').each(function(i, element){
+        test=element.attribs.href;
+        if(test.indexOf("weekly")!=-1){
+         console.log("linksjson push this:", test, i);
+         linksjson.push({"website": test});
+        }
       });
+      // now the linksjson has the links to each parkrun, write the file
+      returnLinks(linksjson);
+    });  // end of the request routine
+  }
+  // ********************* 
+
+  function writeLinks(listOfLinks){
+    fs.writeFile('public/links.json', JSON.stringify(listOfLinks, null, 4), function(err){
+          console.log('File links.json successfully written!');
+        });
   }
 
-var timerFunction = setTimeout(function(){
-  console.log("should be 1 seconds later writing file");
-  fs.writeFileSync('public/output.json', JSON.stringify(json, null, 4));
-  console.log("File written! - Check your output.json file");
-},1000);
-      
-//try end of first timout here
-}, 1500);
 
-console.log("and this is after the subroutine before file send");
+console.log("and this is after all the stuff before the view file send");
 res.sendfile('./public/results.html');
 });
 
