c363fb82bffacbf87d2918df1e3aca7d04615794
spencermeyer <spencer_meyer@hotmail.com>
2017-01-20 21:20:47 +0000

    some more tidying

--- public/injector.js
+++ public/injector.js
@@ -1,16 +1,13 @@
 // Scraping runs!
 
 $( document ).ready(function() {
-  console.log("This is injector.js required in results html");
+  //console.log("This is injector.js required in results html");
 
   var timerFunction1 = setTimeout(function(){
-      //console.log("in first timer");
       var htmlIntro = '<p> Today there were runners from: </p>';
       $.getJSON("links.json", function (links) {
-        //console.log("got links", links[0].website)
         for (i = 0; i < links.length; i++) {
           var runIdentifier = links[i].website.slice(links[i].website.indexOf('uk/')+3, links[i].website.indexOf('/results'));
-          //console.log("runIdentifier", runIdentifier);
           htmlIntro=htmlIntro + '<div class="intro-links"><a href="' + links[i].website + '">';
           htmlIntro=htmlIntro + runIdentifier;
           htmlIntro=htmlIntro + '</a>' + '<span id="more-info' + i + '"></span></div>'
@@ -32,6 +29,20 @@ $( document ).ready(function() {
       htmltoappend = htmltoappend + '<tr class="row"><th>'+'Parkrun'+'</th><th>'+'Date'+'</th><th>' + "Result" + '</th><th>' + 'Parkrunner' + '</th>' + '<th>' + 'Time' + '</th>' + '<th>' + 'Agecat' + '</th>' + '<th>' + 'Agegrade' +'<th>' + 'AgeGradePos' + '</th>' + '<th id="gender">'+'Gender' + '</th>' +  '<th>' + 'GenderPos' + '</th>' + '<th>' + 'Note' + '</th>' + '<th>' + 'Total Runs' + '</th></tr>';
       htmltoappend = htmltoappend + '</thead><tbody>'
 
+      // here want to sort the result
+      result.forEach(function(line, index){ 
+        if(!line.club.includes('Eastleigh RC')){
+          result.splice(index,1);
+        }
+      });
+
+      result.sort(function(a,b) {
+        if(a.agegrade == '') { return +1 };
+        if(a.parkrun !== b.parkrun) { if(a.parkrun < b.parkrun) {return -1} else if(a.parkrun > b.parkrun) {return +1} };
+        //if (parseInt(b.agegrade) > parseInt(a.agegrade)) { return  1} else { return -1};
+        if (parseInt(b.pos) < parseInt(a.pos)) { return  1} else { return -1};
+      });
+
       for (i = 0; i < result.length; i++) {
 
         if(result[i].club == "Eastleigh RC"){
@@ -50,6 +61,7 @@ $( document ).ready(function() {
     });
   //console.log("Injected2 ?");
   },6600);
+
 
   var timerFunction3 = setTimeout(function(){
     var countsHTML = '<table class="table table-bordered table-hover"><th>Run Location</th><th>Number Of Eastleigh Men</th><th>Number of Eastleigh Women</th><th>Total Men</th><th>Total Women</th><th>Total</th>'
