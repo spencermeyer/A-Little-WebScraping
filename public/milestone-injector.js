61f3e08d9cf63da77403359c5379463664214d1c
spencermeyer <spencer_meyer@hotmail.com>
2017-02-07 18:29:54 +0000

    a bit of improvment on the milestones

--- public/milestone-injector.js
+++ public/milestone-injector.js
@@ -2,9 +2,11 @@ $( document ).ready(function() {
   $.getJSON("milestones.json", function (milestones) {
     var htmltoappend = '<table id="milestones" class="table table-bordered table-hover">';
     htmltoappend = htmltoappend + '<thead>';
-    htmltoappend = htmltoappend + '<tr class="row"><th class="justify-left">RunnerName</th><th>Number of Runs</th></tr></thead>';
+    htmltoappend = htmltoappend + '<tr class="row"><th class="justify-left">RunnerName</th><th>Age Cat</th><th>Club</th><th>Number of Runs</th></tr></thead>';
     milestones.forEach(function(milestone){
       htmltoappend = htmltoappend + '<tr class="row">' + '<td>' + milestone.parkrunner + '</td>';
+      htmltoappend = htmltoappend + '<td>' + milestone.agecat + '</td>';
+      htmltoappend = htmltoappend + '<td>' + milestone.club + '</td>';
       htmltoappend = htmltoappend + '<td>' + milestone.TotalRuns + '</td></tr>';
     });
     htmltoappend = htmltoappend + '</table>'
